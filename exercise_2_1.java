2a87285d632c472f21235730b8aea963585f5ad9
judy583 <judyahn0316@gmail.com>
2026-03-05 08:26:04 -0600

    exercise_2_1.java

---
+++ exercise_2_1.java
@@ -0,0 +1,30 @@
+package unit_1_practice;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number = 6;
+
+        System.out.print("Guess a number between 1 and 10: ");
+
+        int guess = Integer.parseInt(input.nextLine());
+
+        if (guess > number) {
+            System.out.println("Too high");
+        }
+        else if (guess == number) {
+            System.out.println("You got it!");
+        }
+        else if (guess < number) {
+            System.out.println("Too low");
+        }
+
+        input.close();
+    }
+
+}
